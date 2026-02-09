d0b931bafb374cdd6951082002283e1d612c16d4
drtienk <drtienk@gmail.com>
2026-02-09 09:28:04 -0500

    0209 icon web

--- abc-bi-app/vite.config.ts
+++ abc-bi-app/vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const base = '/iPVMS-EDU-BI/'
+
 export default defineConfig({
-  plugins: [react()],
-  base: '/iPVMS-EDU-BI/',
+  plugins: [
+    react(),
+    {
+      name: 'html-base-url',
+      transformIndexHtml(html) {
+        return html.replace(/%BASE_URL%/g, base)
+      },
+    },
+  ],
+  base,
 })
 
